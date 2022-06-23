349002486551e4e7a2ca9bce1ec0a9292a4fcc4d
Julius Moeller <moeller.julius2@gmail.com>
2022-06-23 02:27:12 +0200

    WIP Int Query

--- integral_pointing_query.py
+++ integral_pointing_query.py
@@ -7,8 +7,6 @@ heasarc = Heasarc()
 Conf.server.set('https://www.isdc.unige.ch/browse/w3query.pl')
 
 
-
-
 class integral_query:
     def __init__(self, 
             object_name = None, 
@@ -16,7 +14,8 @@ class integral_query:
             radius = None, # Example: "120 arcmin" or "120*u.arcmin"
             mission = "integral_rev3_scw", 
             sortvar = "START_DATE", 
-            resultsmax = 0):
+            resultsmax = 0
+            ):
         
         self.query_dict={}
         
@@ -29,19 +28,61 @@ class integral_query:
 
 
         if not object_name is None and position is None:
-            r = heasarc.query_object(**self.query_dict)
+            self.table = heasarc.query_object(**self.query_dict)
 
         elif object_name is None and not position is None and not radius is None:
-            r = heasarc.query_region(**self.query_dict)
+            self.table = heasarc.query_region(**self.query_dict)
 
         else:
             raise TypeError("Please specify either object_name, or position and radius")
 
-        r.convert_bytestring_to_unicode()
-        p = r.to_pandas()
+        self.table.convert_bytestring_to_unicode()
+        self.table = self.table.to_pandas()
+
+        int_cols = ["SCW_ID", "SCW_VER"]
+        self.table[int_cols] = pd.to_numeric(self.table[int_cols].stack()).unstack()
+
+        flt_cols = ["RA_X", "DEC_X", "_SEARCH_OFFSET", "OBS_ID", "GOOD_SPI", "GOOD_PICSIT", "GOOD_ISGRI", "GOOD_JEMX", "GOOD_JEMX1", "GOOD_JEMX2", "GOOD_OMC", "DSIZE"]
+        self.table[flt_cols] = pd.to_numeric(self.table[flt_cols].stack(), errors="coerce").unstack()
+
+        dat_cols = ["START_DATE", "END_DATE"]
+        self.table[dat_cols] = pd.to_datetime(self.table[dat_cols].stack()).unstack()
+
+        self.filterd_table = self.table.copy()
+        self.SCW_ID_list = self.filterd_table["SCW_ID"]
+    
+    def reset_filter(self):
+        self.filterd_table = self.table.copy()
+        self.SCW_ID_list = self.filterd_table["SCW_ID"]
+
+    def filter(self,
+        SCW_ID = None, # Match Value
+        SCW_VER = None, # Match Value
+        SCW_TYPE = None, # Match Value
+        RA_X = None, # (lower_limit, upper_limit)
+        DEC_X = None, # (lower_limit, upper_limit)
+        START_DATE = None, # (lower_limit, upper_limit)
+        END_DATE = None, # (lower_limit, upper_limit)
+        OBS_ID = None, # Match Value
+        OBS_TYPE = None, # Match Value
+        PS = None, # Match Value
+        PI_NAME = None, # Match Value
+        GOOD_SPI = None, # (lower_limit, upper_limit)
+        GOOD_PICSIT = None, # (lower_limit, upper_limit)
+        GOOD_JMEX = None, # (lower_limit, upper_limit)
+        GOOD_JMEX1 = None, # (lower_limit, upper_limit)
+        GOOD_JMEX2 = None, # (lower_limit, upper_limit)
+        GOOD_OMC = None, # (lower_limit, upper_limit)
+        DSIZE = None, # (lower_limit, upper_limit)
+        _SEARCH_OFFSET = None, # (lower_limit, upper_limit)
+        ):
+
+        self.SCW_ID_list = self.filterd_table["SCW_ID"]
+        return self.SCW_ID_list
+
 
         
-#test2 = integral_query(object_name="Cyg X-1")
+test2 = integral_query(object_name="Cyg X-1", radius="1 degree")
 
 
 test = integral_query(position=SkyCoord('12h29m06.70s +02d03m08.7s', frame='icrs'), radius="1 degree")
